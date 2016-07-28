f19b79b7a9b903598a1f6c9f305d70b13c476585
Jeremiah <jeremiah@ideonic.com>
2016-07-28 18:14:21 +0800

    add redux wip

--- client/main.js
+++ client/main.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
+import { createStore } from 'redux'
 
-const rootNode = document.getElementById('react-container')
+// pass initial state to our reducer function
+const chatReducer = (state = {loading: true, status: 'loading', name: null, messages: [], online: ''}, action) => {
+  switch (action.type) {
+    case 'status':
+      return Object.assign({}, state, { status: action.status })
+    default:
+      return state
+  }
+}
+// create a redux store
+const reduxStore = createStore(chatReducer)
 
-ReactDOM.render(<App socket={io(window.location.host)} />, rootNode)
+const render = () => {
+  ReactDOM.render(<App redux={reduxStore} socket={io(window.location.host)} />, document.getElementById('react-container'))
+}
+
+// subscribe to all changes to store and render
+reduxStore.subscribe(() => {
+  console.log('App State: ', reduxStore.getState())
+  render()
+})
+
+// use dispatch to add a state
+// store.dispatch({ type: 'join' })
